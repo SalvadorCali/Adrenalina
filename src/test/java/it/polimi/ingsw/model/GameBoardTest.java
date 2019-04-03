61dd06df0371bb16e659b78b1b4521ce55c1f055
pitt1095 <10497479@polimi.it>
2019-04-03 15:06:45 +0200

    GameBoardTests + SquareTest

---
+++ src/test/java/it/polimi/ingsw/model/GameBoardTest.java
@@ -0,0 +1,78 @@
+package it.polimi.ingsw.model;
+
+import it.polimi.ingsw.util.Parser;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+class GameBoardTest {
+
+    private List<GameBoard> gameBoards = Parser.createGameBoards();
+    private Player player = new Player();
+    private Position position = new Position(1,1);
+
+    @Test
+    void correctSingleMoveTest(){
+        GameBoard gameBoard = gameBoards.get(0);
+        player.setPosition(position);
+        assertTrue(gameBoard.canMove(player, Direction.DOWN));
+    }
+
+    @Test
+    void correctDoubleMoveTest(){
+
+        GameBoard gameBoard = gameBoards.get(0);
+        player.setPosition(position);
+        assertTrue(gameBoard.canMove(player, Direction.DOWN, Direction.RIGHT));
+    }
+
+    @Test
+    void correctTripleMoveTest(){
+
+        GameBoard gameBoard = gameBoards.get(0);
+        player.setPosition(position);
+        assertTrue(gameBoard.canMove(player, Direction.DOWN, Direction.RIGHT,Direction.RIGHT ));
+    }
+
+    @Test
+    void correctQuadrupleMoveTest(){
+
+        GameBoard gameBoard = gameBoards.get(0);
+        player.setPosition(position);
+        assertTrue(gameBoard.canMove(player, Direction.DOWN, Direction.RIGHT,Direction.RIGHT, Direction.UP ));
+    }
+
+    @Test
+    void firstMoveErrorTest(){
+
+        GameBoard gameBoard = gameBoards.get(0);
+        player.setPosition(position);
+        assertFalse(gameBoard.canMove(player, Direction.RIGHT));
+    }
+
+    @Test
+    void doubleMoveErrorTest(){
+
+        GameBoard gameBoard = gameBoards.get(0);
+        player.setPosition(position);
+        assertFalse(gameBoard.canMove(player, Direction.DOWN, Direction.DOWN));
+    }
+
+    @Test
+    void tripleMoveErrorTest(){
+
+        GameBoard gameBoard = gameBoards.get(0);
+        player.setPosition(position);
+        assertFalse(gameBoard.canMove(player, Direction.DOWN, Direction.RIGHT, Direction.DOWN));
+    }
+
+    @Test
+    void quadrupleMoveErrorTest(){
+
+        GameBoard gameBoard = gameBoards.get(0);
+        player.setPosition(position);
+        assertFalse(gameBoard.canMove(player, Direction.DOWN, Direction.RIGHT, Direction.UP,Direction.LEFT));
+    }
+}
---
+++ src/test/java/it/polimi/ingsw/model/SquareTest.java
@@ -0,0 +1,38 @@
+package it.polimi.ingsw.model;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
+
+class SquareTest {
+
+    Square square = new SpawnPoint(TokenColor.BLUE, Cardinal.ROOM, Cardinal.DOOR, Cardinal.WALL, Cardinal.NONE);
+    Square square2 = new AmmoPoint(TokenColor.YELLOW, Cardinal.DOOR, Cardinal.ROOM, Cardinal.NONE, Cardinal.WALL);
+    Square square3 = new SpawnPoint(TokenColor.GREEN, Cardinal.WALL, Cardinal.NONE, Cardinal.DOOR, Cardinal.ROOM);
+    Square square4 = new AmmoPoint(TokenColor.GREY, Cardinal.NONE, Cardinal.WALL, Cardinal.ROOM, Cardinal.DOOR);
+
+    @Test
+    void canMoveTest(){
+
+        assertTrue(square.canMove(Direction.UP));
+        assertTrue(square.canMove(Direction.DOWN));
+        assertFalse(square.canMove(Direction.RIGHT));
+        assertFalse(square.canMove(Direction.LEFT));
+
+        assertTrue(square2.canMove(Direction.UP));
+        assertTrue(square2.canMove(Direction.DOWN));
+        assertFalse(square2.canMove(Direction.RIGHT));
+        assertFalse(square2.canMove(Direction.LEFT));
+
+        assertFalse(square3.canMove(Direction.UP));
+        assertFalse(square3.canMove(Direction.DOWN));
+        assertTrue(square3.canMove(Direction.RIGHT));
+        assertTrue(square3.canMove(Direction.LEFT));
+
+        assertFalse(square4.canMove(Direction.UP));
+        assertFalse(square4.canMove(Direction.DOWN));
+        assertTrue(square4.canMove(Direction.RIGHT));
+        assertTrue(square4.canMove(Direction.LEFT));
+
+    }
+}
+
