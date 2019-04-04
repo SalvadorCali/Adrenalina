e88a0c85b2e5973733d08730fd0538141771360e
Tommaso Comelli <comelli.tom@gmail.com>
2019-04-04 19:23:19 +0200

    Board and Game Controller update
    main classes about these methods

--- src/main/java/it/polimi/ingsw/controller/GameController.java
+++ src/main/java/it/polimi/ingsw/controller/GameController.java
@@ -1,4 +1,28 @@
 package it.polimi.ingsw.controller;
 
 public class GameController {
+
+    public int timerTurn;
+    public int numPlayers;
+
+    //methods need to be implemented
+    private void countTurn(){
+
+    }
+
+    private void countDeath(){
+
+    }
+
+    private void startAdrenaline(){
+
+    }
+
+    private void endGame(){
+
+    }
+
+    public void calculatePoints(){
+
+    }
 }
