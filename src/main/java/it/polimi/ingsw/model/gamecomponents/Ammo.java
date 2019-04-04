c018c311d98178d2a83de33f19c3ce785bd2e31a
Tommaso Comelli <comelli.tom@gmail.com>
2019-04-04 18:37:26 +0200

    small update ammo.java
    new classes set&get

--- src/main/java/it/polimi/ingsw/model/gamecomponents/Ammo.java
+++ src/main/java/it/polimi/ingsw/model/gamecomponents/Ammo.java
@@ -2,4 +2,12 @@ package it.polimi.ingsw.model.gamecomponents;
 
 public class Ammo {
     private Color color;
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
 }
