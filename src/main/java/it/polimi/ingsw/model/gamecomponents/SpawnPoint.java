ab30b2844b7c0fb8bb233f85ccce41097ac2f4bb
SalvadorCali <andrea.calici@mail.polimi.it>
2019-04-06 15:36:38 +0200

    SpawnPoint tests added

--- src/main/java/it/polimi/ingsw/model/gamecomponents/SpawnPoint.java
+++ src/main/java/it/polimi/ingsw/model/gamecomponents/SpawnPoint.java
@@ -9,12 +9,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SpawnPoint extends Square {
-
     private List<WeaponCard> weapons;
 
     public SpawnPoint(TokenColor color, Cardinal north, Cardinal south, Cardinal west, Cardinal east) {
         super(color, north, south, west, east);
-        weapons = new ArrayList<>();
+        this.weapons = new ArrayList<>();
     }
 
     //getters and setters
@@ -22,8 +21,11 @@ public class SpawnPoint extends Square {
         return weapons;
     }
 
+    public void setWeapons(List<WeaponCard> weapons) {
+        this.weapons = weapons;
+    }
+
     //methods
-    //public void addWeapons
     @Override
     public void grab(ActionInterface actionInterface, int choice) {
         actionInterface.addWeapon(weapons.get(choice));
--- src/test/java/it/polimi/ingsw/model/SpawnPointTest.java
+++ src/test/java/it/polimi/ingsw/model/SpawnPointTest.java
@@ -2,35 +2,36 @@ package it.polimi.ingsw.model;
 
 import it.polimi.ingsw.controller.ActionController;
 import it.polimi.ingsw.model.cards.AmmoCard;
+import it.polimi.ingsw.model.cards.WeaponCard;
 import it.polimi.ingsw.model.cards.effects.ActionInterface;
 import it.polimi.ingsw.model.enums.Cardinal;
 import it.polimi.ingsw.model.enums.Color;
 import it.polimi.ingsw.model.enums.TokenColor;
-import it.polimi.ingsw.model.gamecomponents.Ammo;
-import it.polimi.ingsw.model.gamecomponents.AmmoPoint;
-import it.polimi.ingsw.model.gamecomponents.Game;
-import it.polimi.ingsw.model.gamecomponents.Player;
+import it.polimi.ingsw.model.gamecomponents.*;
 import it.polimi.ingsw.util.Parser;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
 public class SpawnPointTest {
-    /*
     @Test
     public void grabTest(){
-        Game game = new Game(Parser.createGameBoards().get(2), Parser.createWeapons(), Parser.createPowerups(), Parser.createAmmos());
+        Game game = new Game(Parser.createGameBoards().get(0), Parser.createWeapons(), Parser.createPowerups(), Parser.createAmmos());
         ActionInterface actionInterface = new ActionController(game);
         Player player = new Player(TokenColor.YELLOW);
         game.setCurrentPlayer(player);
-        AmmoPoint square = new AmmoPoint(TokenColor.BLUE, Cardinal.NONE, Cardinal.NONE, Cardinal.NONE, Cardinal.NONE);
-        AmmoCard ammoCard = new AmmoCard(new Ammo(Color.RED), new Ammo(Color.BLUE));
-        square.setAmmoCard(ammoCard);
-        square.grab(actionInterface, 0);
+        Deck weaponCards = Parser.createWeapons();
+        List<WeaponCard> weapons = new ArrayList<>();
+        weapons.add((WeaponCard)weaponCards.get(0));
+        weapons.add((WeaponCard)weaponCards.get(1));
+        weapons.add((WeaponCard)weaponCards.get(2));
+        SpawnPoint square = new SpawnPoint(TokenColor.BLUE, Cardinal.NONE, Cardinal.NONE, Cardinal.NONE, Cardinal.NONE);
+        square.setWeapons(weapons);
+        square.grab(actionInterface, 2);
 
-        assertEquals(Color.RED, player.getAmmoBox().get(0).getColor());
-        assertEquals(Color.BLUE, player.getAmmoBox().get(1).getColor());
-        assertEquals(1, player.getPowerups().size());
+        assertEquals(weapons.get(2), player.getWeapons().get(0));
     }
-    */
 }
