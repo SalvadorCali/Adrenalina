4a406d3a2777d3063904fbc734bbef42fd57fcab
SalvadorCali <andrea.calici@mail.polimi.it>
2019-05-01 13:04:22 +0200

    Update Server ip address

--- src/main/java/it/polimi/ingsw/network/server/Server.java
+++ src/main/java/it/polimi/ingsw/network/server/Server.java
@@ -4,17 +4,20 @@ import it.polimi.ingsw.network.ConnectionManager;
 import it.polimi.ingsw.util.Printer;
 
 import java.io.IOException;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
 
 public class Server {
-    public static void main(String[] args) {
-        try {
-            String ip = InetAddress.getLocalHost().getHostAddress();
-            Printer.println("[SERVER]Current ip address: " + ip);
-            System.setProperty("java.rmi.server.hostname", ip);
-        } catch (UnknownHostException e) {
-            Printer.err(e);
+    public static void main(String[] args) throws UnknownHostException, SocketException {
+        //1st method
+        List<InetAddress> addresses = getAddresses();
+        Printer.println("[SERVER]Current ip addresses:");
+        addresses.forEach(a -> Printer.println("    " + a));
+        if(!addresses.isEmpty()){
+            //Printer.println(addresses.get(0).toString().substring(1));
+            System.setProperty("java.rmi.server.hostname", addresses.get(0).toString());
         }
         try {
             ConnectionManager connection = new ConnectionManager();
@@ -22,5 +25,21 @@ public class Server {
         } catch (IOException e) {
             Printer.err(e);
         }
+    }
+
+    private static List<InetAddress> getAddresses() throws SocketException{
+        Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+        List<InetAddress> addressesList = new ArrayList<>();
+        while(networkInterfaces.hasMoreElements()) {
+            NetworkInterface networkInterface = networkInterfaces.nextElement();
+            Enumeration<InetAddress> addresses = networkInterface.getInetAddresses();
+            while(addresses.hasMoreElements()){
+                InetAddress address = addresses.nextElement();
+                if(address instanceof Inet4Address && !address.isLoopbackAddress()){
+                    addressesList.add(address);
+                }
+            }
+        }
+        return addressesList;
     }
 }
