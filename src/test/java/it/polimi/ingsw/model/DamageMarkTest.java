cb3ce046c9d7ca7a4b2f92002a5bc2feecc2c93c
pitt1095 <10497479@polimi.it>
2019-05-08 16:41:31 +0200

    minor changes

--- src/test/java/it/polimi/ingsw/model/DamageMarkTest.java
+++ src/test/java/it/polimi/ingsw/model/DamageMarkTest.java
@@ -16,27 +16,15 @@ import static junit.framework.TestCase.assertTrue;
 public class DamageMarkTest {
 
     private GameController gameController = new GameController();
+    private Player currentPlayer = new Player(TokenColor.GREEN);
+    private Player victim = new Player(TokenColor.BLUE);
+
 
 
     @Test
-    public void lockRifleBasicTest() {
+    public void lockRifleCanUseEffectTest() {
 
-        ClientData clientData = gameController.getActionInterface().getClientData();
-
-        //currentPlayerSetup
-        Player currentPlayer = new Player(TokenColor.GREEN);
-        gameController.getGame().getBoard().generatePlayer(0,0,currentPlayer);
-        gameController.getGame().getPlayers().add(currentPlayer);
-        clientData.setCurrentPlayer(currentPlayer);
-        currentPlayer.increaseAmmoNumber(Color.BLUE);
-        currentPlayer.increaseAmmoNumber(Color.BLUE);
-
-        //victimSetup
-        Player victim = new Player(TokenColor.BLUE);
-        gameController.getGame().getBoard().generatePlayer(1,0,victim);
-        gameController.getGame().getPlayers().add(victim);
-        clientData.setVictim(victim);
-
+        playerSetup();
 
         Effect lockRifle = new DamageMarkEffect("Lock Rifle", 2, 1, 0, 2, 0);
         assertTrue(lockRifle.canUseEffect(gameController.getActionInterface()));
@@ -55,5 +43,23 @@ public class DamageMarkTest {
 
         gameController.getGame().getBoard().move(1,1,currentPlayer);
         assertFalse(lockRifle.canUseEffect(gameController.getActionInterface()));
+    }
+
+    void playerSetup(){
+
+        ClientData clientData = gameController.getActionInterface().getClientData();
+
+        //currentPlayerSetup
+        gameController.getGame().getBoard().generatePlayer(0,0,currentPlayer);
+        gameController.getGame().getPlayers().add(currentPlayer);
+        clientData.setCurrentPlayer(currentPlayer);
+        currentPlayer.increaseAmmoNumber(Color.BLUE);
+        currentPlayer.increaseAmmoNumber(Color.BLUE);
+
+        //victimSetup
+        gameController.getGame().getBoard().generatePlayer(1,0,victim);
+        gameController.getGame().getPlayers().add(victim);
+        clientData.setVictim(victim);
+
     }
 }
