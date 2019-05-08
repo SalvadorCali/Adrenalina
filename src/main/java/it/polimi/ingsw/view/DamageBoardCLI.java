2221a389f4af845380fc2f43d5549d4b0559efa7
tc <comelli.tom@gmail.com>
2019-05-08 16:59:47 +0200

    change mapCLI
    changed layout

--- src/main/java/it/polimi/ingsw/view/DamageBoardCLI.java
+++ src/main/java/it/polimi/ingsw/view/DamageBoardCLI.java
@@ -13,7 +13,7 @@ public class DamageBoardCLI {
 
     public static final String SPACE = " ";  // Space
     public static final String RESET = "\033[0m";  // Text Reset
-    public static final Integer MAX_DAMAGE = 12;  // Text Reset
+    public static final Integer MAX_DAMAGE = 12;
     public static final String BLACK = "\033[0;30m";   // BLACK
     public static final String RED = "\033[0;31m";     // RED
     public static final String GREEN = "\033[0;32m";   // GREEN
