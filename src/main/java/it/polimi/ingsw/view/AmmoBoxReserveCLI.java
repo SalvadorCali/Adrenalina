c7667fc36a3b67f29d3dd985afcc97567b53040e
tc <comelli.tom@gmail.com>
2019-05-09 17:52:06 +0200

    update AmmoBox/AmmoReserve

--- src/main/java/it/polimi/ingsw/view/AmmoBoxReserveCLI.java
+++ src/main/java/it/polimi/ingsw/view/AmmoBoxReserveCLI.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 
 
-public class AmmoBoxCLI {
+public class AmmoBoxReserveCLI {
 
     public static final String SPACE = " ";  // Space
     public static final Integer RESERVE_LENGTH = 9;
@@ -20,14 +20,16 @@ public class AmmoBoxCLI {
 
 
     private List<Ammo> ammoBox;
+    private List<Ammo> ammoReserve;
 
-    public AmmoBoxCLI(Player player){
+    public AmmoBoxReserveCLI(Player player){
         
         ammoBox = player.getAmmoBox();
+        ammoReserve = player.getAmmoReserve();
     }
 
 
-    public void printAmmoReserve(){
+    public void printAmmoBox(){
 
         String ammoColor[] = new String[RESERVE_LENGTH];
         String ammoVal[] = new String[RESERVE_LENGTH];
@@ -49,7 +51,36 @@ public class AmmoBoxCLI {
         }
 
 
+        Printer.println("Your Ammo Box:");
+        Printer.println(SPACE + "_________________");
+        Printer.println("|" + ammoColor[0] + ammoVal[0] + SPACE + ammoColor[1] + ammoVal[1] + SPACE + ammoColor[2] + ammoVal[2] + SPACE + ammoColor[3] + ammoVal[3] + SPACE + ammoColor[4] + ammoVal[4] + SPACE + ammoColor[5] + ammoVal[5] + SPACE + ammoColor[6] + ammoVal[6] + SPACE + ammoColor[7] + ammoVal[7] + SPACE + ammoColor[8] + ammoVal[8] + GREY + "|" + RESET);
+        Printer.println(SPACE + "‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾");
+    }
 
+
+    public void printAmmoReserve(){
+
+        String ammoColor[] = new String[RESERVE_LENGTH];
+        String ammoVal[] = new String[RESERVE_LENGTH];
+
+
+        //give color to ammo
+        for (int i = 0; i< RESERVE_LENGTH; i++){
+            ammoColor[i] = Converter.fromColorToCLIColor(ammoReserve.get(i).getColor());
+        }
+
+        for(int i = 0; i < RESERVE_LENGTH; i++){
+
+            ammoVal[i] = AMMO;
+
+            if(ammoColor[i].isEmpty()){
+
+                ammoVal[i] = SPACE;
+            }
+        }
+
+
+        Printer.println("Your Ammo Reserve:");
         Printer.println(SPACE + "_________________");
         Printer.println("|" + ammoColor[0] + ammoVal[0] + SPACE + ammoColor[1] + ammoVal[1] + SPACE + ammoColor[2] + ammoVal[2] + SPACE + ammoColor[3] + ammoVal[3] + SPACE + ammoColor[4] + ammoVal[4] + SPACE + ammoColor[5] + ammoVal[5] + SPACE + ammoColor[6] + ammoVal[6] + SPACE + ammoColor[7] + ammoVal[7] + SPACE + ammoColor[8] + ammoVal[8] + GREY + "|" + RESET);
         Printer.println(SPACE + "‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾");
