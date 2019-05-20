08ce0e434705125eb0a714423edf9a87feb6448d
pitt1095 <10497479@polimi.it>
2019-05-20 12:53:09 +0200

    Update AdditionalSquareDamage.java

--- src/main/java/it/polimi/ingsw/model/cards/effects/weapons/singleaddictions/AdditionalSquareDamage.java
+++ src/main/java/it/polimi/ingsw/model/cards/effects/weapons/singleaddictions/AdditionalSquareDamage.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.model.cards.effects.weapons.singleaddictions;
 
 import it.polimi.ingsw.model.cards.effects.ActionInterface;
+import it.polimi.ingsw.model.cards.effects.Effect;
 
 public class AdditionalSquareDamage extends SingleAddictionEffect{
 
@@ -17,20 +18,35 @@ public class AdditionalSquareDamage extends SingleAddictionEffect{
     private boolean canUse;
 
 
+    public AdditionalSquareDamage(String effectName, int damagePower, int redAmmos, int blueAmmos, int yellowAmmos, Effect effect){
+
+        this.effectName = effectName;
+        this.damagePower = damagePower;
+        this.redAmmos = redAmmos;
+        this.blueAmmos = blueAmmos;
+        this.yellowAmmos = yellowAmmos;
+        super.effect = effect;
+
+    }
+
+
     @Override
     public boolean canUseEffect(ActionInterface actionInterface) {
+
+        setData(actionInterface);
 
         if(basicFirst){
             canUse = super.effect.canUseEffect(actionInterface) && actionInterface.ammoControl(redAmmos, blueAmmos, yellowAmmos);
             if(canUse)
-                canUse = actionInterface.isVisibleDifferentSquare(x, y);// || actionInterface.sameSquare(actionInterface.getCurrentPlayer().getColor()); //actionInterface.getPlayerPos()
+                canUse = actionInterface.isVisibleDifferentSquare(x, y);
         }else{
-            //canUse = actionInterface.ammoControl(redAmmos, blueAmmos, yellowAmmos) && (actionInterface.isVisibleDifferentSquare(x, y) || actionInterface.sameSquare(actionInterface.getCurrentPlayer().getColor())); //actionInterface.getPlayerPos()
+            canUse = actionInterface.ammoControl(redAmmos, blueAmmos, yellowAmmos) && (actionInterface.isVisibleDifferentSquare(x, y) || (x == actionInterface.getCurrentPlayer().getPosition().getX() && y == actionInterface.getCurrentPlayer().getPosition().getY()));
                 if(canUse)
                     canUse = super.effect.canUseEffect(actionInterface);
         }
         return canUse;
     }
+
 
     @Override
     public void useEffect(ActionInterface actionInterface) {
@@ -40,5 +56,10 @@ public class AdditionalSquareDamage extends SingleAddictionEffect{
         if(effectName.equals("Rocket Launcher"))
             actionInterface.playerDamage(actionInterface.getVictim().getColor(), damagePower);
         actionInterface.updateAmmoBox(redAmmos, blueAmmos, yellowAmmos);
+    }
+
+    private void setData(ActionInterface actionInterface){
+        this.x = actionInterface.getClientData().getSquare().getX();
+        this.y = actionInterface.getClientData().getSquare().getY();
     }
 }
