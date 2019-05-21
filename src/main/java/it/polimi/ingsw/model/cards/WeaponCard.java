8858bdf08f7edadc7abadaa411692c2c06686092
pitt1095 <10497479@polimi.it>
2019-05-21 16:36:54 +0200

    Update WeaponCard.java

--- src/main/java/it/polimi/ingsw/model/cards/WeaponCard.java
+++ src/main/java/it/polimi/ingsw/model/cards/WeaponCard.java
@@ -1,17 +1,54 @@
 package it.polimi.ingsw.model.cards;
 
+import it.polimi.ingsw.model.cards.effects.weapons.basiceffects.DamageMarkEffect;
+import it.polimi.ingsw.model.cards.effects.weapons.basiceffects.SquareDamageEffect;
+import it.polimi.ingsw.model.cards.effects.weapons.singleaddictions.AdditionalSquareDamage;
+import it.polimi.ingsw.model.cards.effects.weapons.singleaddictions.AdditionalTarget;
 import it.polimi.ingsw.model.enums.Color;
 import it.polimi.ingsw.model.cards.effects.Effect;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 public class WeaponCard extends Card{
 
     private int weaponId;
-    private List<Effect> effects;
 
-    public WeaponCard(String name, Color color) {
+    private int grabRedAmmos, grabBlueAmmos, grabYellowAmmos;
+
+    private int reloadRedAmmos, reloadBlueAmmos, reloadYellowAmmos;
+
+    private List<Effect> effects = new ArrayList<>();
+
+    public WeaponCard(String name, Color color, int grabRedAmmos, int grabBlueAmmos, int grabYellowAmmos, int reloadRedAmmos, int reloadBlueAmmos, int reloadYellowAmmos) {
+
         super(name, color);
+        this.grabRedAmmos = grabRedAmmos;
+        this.grabBlueAmmos = grabBlueAmmos;
+        this.grabYellowAmmos = grabYellowAmmos;
+        this.reloadRedAmmos = reloadRedAmmos;
+        this.reloadBlueAmmos = reloadBlueAmmos;
+        this.reloadYellowAmmos = reloadYellowAmmos;
+    }
+
+    private void setEffects(String name){
+
+        switch (name){
+            case ("LOCK RIFLE"):
+                Effect lockRifle = new DamageMarkEffect("Lock Rifle", 2,1, 0,0,0);
+                Effect lockRifleAdd = new AdditionalTarget("Lock Rifle", 0, 1,1,0,0, lockRifle);
+                break;
+            case ("ELECTROSCYTHE"):
+                Effect electroscythe1 = new SquareDamageEffect("Electroscythe", 1,0, 0,0,0);
+                Effect electroscythe2 = new SquareDamageEffect("Electroscythe", 2,0,1,1,0);
+                break;
+            case ("MACHINE GUN"):
+                Effect machineGun = new DamageMarkEffect("Machine Gun", 1,0, 0,0,0);
+                Effect machineGunAdd = new AdditionalTarget("Machine Gun", 1,0,0,0,1, machineGun);
+                Effect machineGunAdd2 = new AdditionalTarget("Machine Gun Double", 1,0,0,1,0, machineGunAdd);
+                break;
+        }
+
     }
 }
