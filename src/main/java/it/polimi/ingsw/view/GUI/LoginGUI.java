efaf4ac106b3b9266de6d3459db67f3bba3a212e
tc <comelli.tom@gmail.com>
2019-05-21 14:43:09 +0200

    loginGUI and mapGUI
    first implementation

---
+++ src/main/java/it/polimi/ingsw/view/GUI/LoginGUI.java
@@ -0,0 +1,204 @@
+package it.polimi.ingsw.view.GUI;
+
+import it.polimi.ingsw.network.ConnectionInterface;
+import it.polimi.ingsw.network.client.ClientInterface;
+import it.polimi.ingsw.network.client.rmi.RMIClient;
+import it.polimi.ingsw.network.client.socket.SocketClient;
+import it.polimi.ingsw.network.server.rmi.RMIServerInterface;
+import it.polimi.ingsw.util.Config;
+import it.polimi.ingsw.util.Converter;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.URL;
+import java.rmi.NotBoundException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.ResourceBundle;
+
+import static java.lang.Thread.sleep;
+
+
+public class LoginGUI extends Application implements Initializable {
+
+    @FXML
+    RadioButton socketButton;
+
+    @FXML
+    RadioButton rmiButton;
+
+    @FXML
+    TextField nicknameField;
+
+    @FXML
+    TextField addressField;
+
+    @FXML
+    TextField colorField;
+
+    @FXML
+    Label statusConnectionLabel;
+
+    @FXML
+    Button loginButton;
+
+    @FXML
+    Label connectionErrorLabel;
+
+    private String playerName;
+    private String address;
+    private String colorPlayer;
+    boolean isRunning = true;
+    private ClientInterface client;
+    private Socket socket;
+    private RMIServerInterface serverRMI;
+    private ObjectInputStream objectInputStream;
+    private ObjectOutputStream objectOutputStream;
+    private ChoosePowerup choosePowerup = new ChoosePowerup();
+
+    public synchronized void start(Stage primaryStage) throws Exception {
+
+        Parent adrenalineLog = FXMLLoader.load(getClass().getClassLoader().getResource("LoginGUI.fxml"));
+        Scene scene = new Scene(adrenalineLog, 600, 470);
+
+        primaryStage.setTitle("Adrenaline Login");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
+
+    public synchronized void clickButton(String playerName, String address, String colorPlayer){
+
+        if(rmiButton.isSelected()){
+            connectToRMI(playerName, address, colorPlayer);
+        }
+
+        if(socketButton.isSelected()){
+            connectToSocket(playerName, address, colorPlayer);
+        }
+
+        if(!rmiButton.isSelected() && !socketButton.isSelected()){
+            connectionErrorLabel.setText("Choose connection method");
+        }
+    }
+
+    public synchronized void connectToRMI(String name, String host, String color){
+
+        try{
+            client = new RMIClient();
+            client.start();
+            client.login(name, Converter.fromStringToTokenColor(color));
+
+            Registry registry = LocateRegistry.getRegistry(host, Config.RMI_FREE_PORT);
+            ConnectionInterface connection = (ConnectionInterface) registry.lookup("server");
+
+        } catch (NotBoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public synchronized void connectToSocket(String name, String host, String color){
+
+        System.out.println(name + host + color);
+
+        try {
+            client = new SocketClient();
+            client.start();
+            client.login(name, Converter.fromStringToTokenColor(color));
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        Thread thread = new Thread(this::handleThread);
+        thread.setDaemon(true);
+        thread.start();
+
+        statusConnectionLabel.setText("Offline");
+    }
+
+    public void handleThread(){
+
+        while(isRunning){
+
+            Platform.runLater(() -> {
+
+                loginButton.setOnAction(actionEvent ->  {
+
+                    statusConnectionLabel.setText("Waiting...");
+                    tryConnection();
+                });
+
+            });
+
+
+            try{
+
+                Thread.sleep(2000);
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public synchronized void tryConnection(){
+
+        playerName = nicknameField.getText();
+        address = addressField.getText();
+        colorPlayer = colorField.getText();
+
+        if(playerName.equals("")){
+            connectionErrorLabel.setText("Userfield can't be empty");
+        }
+
+        else if(address.equals("")){
+            connectionErrorLabel.setText("Ip can't be empty");
+        }
+
+        else if(colorPlayer.equals("")){
+            connectionErrorLabel.setText("Color can't be empty");
+        }
+
+        else{
+            clickButton(playerName, address, colorPlayer);
+        }
+
+    }
+
+    public void dispose(){
+
+        isRunning = false;
+        statusConnectionLabel.setText("Closing Connection");
+    }
+
+
+
+    public synchronized void ifConnected(){
+
+        statusConnectionLabel.setText("Online");
+        //implement launch choosePowerup
+
+    }
+}
+
---
+++ src/main/java/it/polimi/ingsw/view/GUI/MapGUI.java
@@ -0,0 +1,191 @@
+package it.polimi.ingsw.view.GUI;
+
+import it.polimi.ingsw.controller.GameController;
+import it.polimi.ingsw.model.enums.BoardType;
+import it.polimi.ingsw.model.gamecomponents.Square;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MapGUI extends Application implements Initializable {
+
+    @FXML
+    private ImageView mapImage;
+
+    @FXML
+    private ImageView weaponRed1;
+
+    @FXML
+    private ImageView weaponRed2;
+
+    @FXML
+    private ImageView weaponRed3;
+
+    @FXML
+    private ImageView weaponYellow1;
+
+    @FXML
+    private ImageView weaponYellow2;
+
+    @FXML
+    private ImageView weaponYellow3;
+
+    @FXML
+    private ImageView weaponBlue1;
+
+    @FXML
+    private ImageView weaponBlue2;
+
+    @FXML
+    private ImageView weaponBlue3;
+
+    @FXML
+    private ImageView deckPowerup;
+
+    @FXML
+    private ImageView deckWeapon;
+
+    @FXML
+    private ImageView deckAmmo;
+
+    @FXML
+    private HBox hboxDeath;
+
+    @FXML
+    private GridPane gridBoard;
+
+    @FXML
+    private GridPane grid00;
+
+    @FXML
+    private GridPane grid10;
+
+    @FXML
+    private GridPane grid20;
+
+    @FXML
+    private GridPane grid30;
+
+    @FXML
+    private GridPane grid01;
+
+    @FXML
+    private GridPane grid11;
+
+    @FXML
+    private GridPane grid02;
+
+    @FXML
+    private GridPane grid12;
+
+    @FXML
+    private GridPane grid21;
+
+    @FXML
+    private GridPane grid22;
+
+    @FXML
+    private GridPane grid32;
+
+    @FXML
+    private GridPane grid31;
+
+    @FXML
+    private ImageView bannerShoot;
+
+    @FXML
+    private ImageView bannerGrab;
+
+    @FXML
+    private ImageView bannerEndTurn;
+
+    @FXML
+    private ImageView upArrow;
+
+    @FXML
+    private ImageView rightArrow;
+
+    @FXML
+    private ImageView downArrow;
+
+    @FXML
+    private ImageView leftArrow;
+
+    @FXML
+    private ImageView bannerShowWeapon;
+
+    @FXML
+    private ImageView bannerShowDamage1;
+
+    @FXML
+    private Label playerTurnLabel;
+
+    private static final int ROWS = 3;
+    private static final int COLUMNS = 4;
+    private static final int NUM_SQUARES=12;
+    private static final int MAX_NUM_PLAYER=5;
+
+    private GameController gameController;
+    private String currentPlayer;
+    private BoardType boardType;
+    private Square[][] arena = new Square[ROWS][COLUMNS];
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+
+        Parent parent = FXMLLoader.load(getClass().getClassLoader().getResource("MapGUI.fxml"));
+        Scene scene = new Scene(parent, 1189, 710);
+
+        stage.setScene(scene);
+        stage.setTitle("Main Board");
+        stage.show();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        setMapImage();
+        updatePlayerTurnLabel();
+    }
+
+    public void updatePlayerTurnLabel(){
+
+        Platform.runLater(() -> {
+
+            currentPlayer = gameController.getGame().getCurrentPlayer().getUsername();
+            playerTurnLabel.setText(currentPlayer);
+        });
+    }
+
+
+    public void setMapImage(){
+
+        //fill
+    }
+
+
+    public void placePlayers(){
+
+        arena = gameController.getGame().getBoard().getArena();
+
+        if(!arena[0][0].getPlayers().isEmpty()){
+            for(int index = 0; index < arena[0][0].getPlayers().size(); index ++){
+
+              
+            }
+        }
+    }
+}
