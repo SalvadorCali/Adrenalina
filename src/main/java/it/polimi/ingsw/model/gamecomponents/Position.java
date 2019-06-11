5d2e1b8757ba7cefc110174f7fec28ceeecf1cf7
pitt1095 <10497479@polimi.it>
2019-06-11 13:27:51 +0200

    Javadoc

--- src/main/java/it/polimi/ingsw/model/gamecomponents/Position.java
+++ src/main/java/it/polimi/ingsw/model/gamecomponents/Position.java
@@ -2,27 +2,51 @@ package it.polimi.ingsw.model.gamecomponents;
 
 import java.io.Serializable;
 
+/**
+ * class which represents the position of a player in the arena.
+ */
+
 public class Position implements Serializable {
     private int x;
     private int y;
 
+    /**
+     * constructor of the Position class.
+     * @param x represents the row.
+     * @param y represents the column.
+     */
     public Position(int x, int y){
         this.x = x;
         this.y = y;
     }
 
+    /**
+     *  getter of the row.
+     */
     public int getX() {
         return x;
     }
 
+    /**
+     * setter of the row
+     * @param x indicates the chosen row to set.
+     */
     public void setX(int x) {
         this.x = x;
     }
 
+    /**
+     * getter of the column.
+     * @return the column
+     */
     public int getY() {
         return y;
     }
 
+    /**
+     * setter of the column.
+     * @param y indicates the chosen column.
+     */
     public void setY(int y) {
         this.y = y;
     }
--- src/main/java/it/polimi/ingsw/model/gamecomponents/SpawnPoint.java
+++ src/main/java/it/polimi/ingsw/model/gamecomponents/SpawnPoint.java
@@ -11,30 +11,59 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * class which represents the spawn point squares in the arena.
+ */
 public class SpawnPoint extends Square implements Serializable {
+
     private List<WeaponCard> weapons;
 
+    /**
+     * constructor of the SpawnPoint class.
+     * @param color indicates the color of the spawn point.
+     * @param north indicates the cardinal on the upper side of the square.
+     * @param south indicates the cardinal on the lower side of the square.
+     * @param west indicates the cardinal on the left side of the square
+     * @param east indicates the cardinal on the right side of the square.
+     */
     public SpawnPoint(TokenColor color, Cardinal north, Cardinal south, Cardinal west, Cardinal east) {
         super(color, north, south, west, east, true);
         this.weapons = new ArrayList<>();
     }
 
-    //getters and setters
+    /**
+     * getter for the list of weapons present in the spawn point.
+     * @return the list of weapons present in the spawn point.
+     */
     @Override
     public List<WeaponCard> getWeapons() {
         return weapons;
     }
 
+    /**
+     * setter for the list of weapons present in the spawn point.
+     * @param weapons chosen weapons to set in the spawn point.
+     */
     public void setWeapons(List<WeaponCard> weapons) {
         this.weapons = weapons;
     }
 
-    //methods
+    /**
+     * verifies if a weapon in the spawn point can be grabbed.
+     * @param actionInterface contains the methods to access the game.
+     * @param choice int which indicates the choice of the player.
+     * @return the result of the control, true if the weapon can be grabbed, false if can't.
+     */
     @Override
     public boolean canGrab(ActionInterface actionInterface, int choice){
         return choice >= 1 && choice <= 3 && !isEmpty() && weapons.get(choice - 1).ammoControl(actionInterface.getCurrentPlayer());
     }
 
+    /**
+     * grabs the chosen weapon from the spawn point.
+     * @param actionInterface contains the methods to access the game.
+     * @param choice int which indicates the choice of the player.
+     */
     @Override
     public void grab(ActionInterface actionInterface, int choice) {
         weapons.get(choice - 1).load();
@@ -44,6 +73,10 @@ public class SpawnPoint extends Square implements Serializable {
         setEmpty(true);
     }
 
+    /**
+     * fills the spawn point with new weapons.
+     * @param actionInterface contains the methods to access the game.
+     */
     @Override
     public void fill(ActionInterface actionInterface){
         int size = weapons.size();
@@ -62,16 +95,28 @@ public class SpawnPoint extends Square implements Serializable {
         */
     }
 
+    /**
+     * fills the spawn point with a new weapon
+     * @param weapon indicates the weapon to add in the spawn point
+     */
     @Override
     public void drop(Card weapon){
         weapons.add((WeaponCard)weapon);
     }
 
+    /**
+     * getter of the ammo card present in the square(Never Used)
+     * @return always null
+     */
     @Override
     public AmmoCard getAmmoCard() {
         return null;
     }
 
+    /**
+     * controls it the square is an active square.
+     * @return true, cause the spawn point is an active square.
+     */
     @Override
     public boolean isActive(){
         return true;
