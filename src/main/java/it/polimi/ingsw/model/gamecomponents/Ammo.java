bd44043e95a9f5aad9f2b15ce1fab585813f77cf
pitt1095 <10497479@polimi.it>
2019-06-11 10:33:28 +0200

    Update Ammo.java

--- src/main/java/it/polimi/ingsw/model/gamecomponents/Ammo.java
+++ src/main/java/it/polimi/ingsw/model/gamecomponents/Ammo.java
@@ -5,21 +5,40 @@ import it.polimi.ingsw.util.Converter;
 
 import java.io.Serializable;
 
+/**
+ * class that represents the ammo components
+ */
 public class Ammo implements Serializable {
     private Color color;
 
+    /**
+     * constructor of the ammo class
+     * @param color indicates the color chosen to construct the Ammo
+     */
     public Ammo(Color color){
         this.color = color;
     }
 
+    /**
+     * setter of the ammo color.
+     * @param color indicates the color chosen to set the ammo color.
+     */
     public void setColor(Color color) {
         this.color = color;
     }
 
+    /**
+     * getter of the ammo color.
+     * @return the color of the ammo.
+     */
     public Color getColor() {
         return color;
     }
 
+    /**
+     * toString of the Color class.
+     * @return the printable string.
+     */
     public String toString(){
         StringBuilder ammo = new StringBuilder();
         ammo.append("\n\033[0;34m _ _ ");
