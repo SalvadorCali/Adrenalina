209b911e4869196b92a355378ff88d432702edf7
Tommaso Comelli <comelli.tom@gmail.com>
2019-06-20 11:06:36 +0200

    Update ScorePopup.java

--- src/main/java/it/polimi/ingsw/view/gui/ScorePopup.java
+++ src/main/java/it/polimi/ingsw/view/gui/ScorePopup.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.view.gui;
 
 import it.polimi.ingsw.model.enums.TokenColor;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
@@ -21,31 +22,33 @@ public class ScorePopup {
 
 
     public void setScore(Map<TokenColor, Integer> score) {
+        Platform.runLater(() -> {
 
-        Integer numPlayer = score.size();
+            Integer numPlayer = score.size();
 
-        if(numPlayer >= 2){
+            if (numPlayer >= 2) {
 
-            firstPlayerScoreNum.setText(String.valueOf((score.keySet().toArray()[0])));
-            secondPlayerScoreNum.setText(String.valueOf((score.keySet().toArray()[1])));
-        }
+                firstPlayerScoreNum.setText(String.valueOf((score.keySet().toArray()[0])));
+                secondPlayerScoreNum.setText(String.valueOf((score.keySet().toArray()[1])));
+            }
 
-        if(numPlayer >= 3){
-            thirdScoreLabel.setVisible(true);
-            thirdPlayerScoreNum.setVisible(true);
-            thirdPlayerScoreNum.setText(String.valueOf((score.keySet().toArray()[2])));
-        }
+            if (numPlayer >= 3) {
+                thirdScoreLabel.setVisible(true);
+                thirdPlayerScoreNum.setVisible(true);
+                thirdPlayerScoreNum.setText(String.valueOf((score.keySet().toArray()[2])));
+            }
 
-        if(numPlayer >= 4){
-            fourthScoreLabel.setVisible(true);
-            fourthPlayerScoreNum.setVisible(true);
-            fourthPlayerScoreNum.setText(String.valueOf((score.keySet().toArray()[3])));
-        }
+            if (numPlayer >= 4) {
+                fourthScoreLabel.setVisible(true);
+                fourthPlayerScoreNum.setVisible(true);
+                fourthPlayerScoreNum.setText(String.valueOf((score.keySet().toArray()[3])));
+            }
 
-        if (numPlayer == 5){
-            fifthScoreLabel.setVisible(true);
-            fifthPlayerScoreNum.setVisible(true);
-            fifthPlayerScoreNum.setText(String.valueOf((score.keySet().toArray()[4])));
-        }
+            if (numPlayer == 5) {
+                fifthScoreLabel.setVisible(true);
+                fifthPlayerScoreNum.setVisible(true);
+                fifthPlayerScoreNum.setText(String.valueOf((score.keySet().toArray()[4])));
+            }
+        });
     }
 }
